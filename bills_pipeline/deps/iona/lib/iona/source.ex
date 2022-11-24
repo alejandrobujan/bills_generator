6a1a146af525877e1320ffc6af8877e4e8d1e252
Jorge Hermo <jorge.hermo.gonzalez@udc.es>
2022-11-24 19:39:18 +0100

    remove: backup pipeline

--- bills_pipeline/deps/iona/lib/iona/source.ex
+++
@@ -1,7 +0,0 @@
-defmodule Iona.Source do
-  @moduledoc false
-
-  defstruct [:path, :content, :include]
-
-  @type t :: %__MODULE__{path: Path.t(), content: Iona.tex_t(), include: [Path.t()]}
-end
--- bills_pipeline/test/generacion_facturas_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GeneracionFacturasTest do
-  use ExUnit.Case
-  doctest GeneracionFacturas
-
-  test "greets the world" do
-    assert GeneracionFacturas.hello() == :world
-  end
-end
