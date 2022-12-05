04d6b7e215964bbc1e5b16637cb49d142d4b132e
Jorge Hermo <jorge.hermo.gonzalez@udc.es>
2022-12-05 14:35:31 +0100

    change error_msg to text in db

--- backend/apps/bills_generator/priv/repo/migrations/20221120204802_create_bill.exs
+++ backend/apps/bills_generator/priv/repo/migrations/20221120204802_create_bill.exs
@@ -4,10 +4,10 @@ defmodule BillsGenerator.Repository.Repo.Migrations.CreateBill do
   def change do
     create table(:bill) do
       add(:user, :string)
-      add(:title, :string)
+      add(:title, :text)
       add(:pdf, :binary)
       add(:error, :boolean)
-      add(:error_msg, :string)
+      add(:error_msg, :text)
       timestamps()
     end
   end
