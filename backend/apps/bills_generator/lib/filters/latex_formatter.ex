c84d1597e19ae17fd3ee0ce4897490fbf6c48de8
Sergio Marcos Vázquez <sergio.marcosv@udc.es>
2022-12-11 12:39:01 +0100

    changes in bill design

--- backend/apps/bills_generator/lib/filters/latex_formatter.ex
+++ backend/apps/bills_generator/lib/filters/latex_formatter.ex
@@ -15,20 +15,22 @@ defmodule BillsGenerator.Filters.LatexFormatter do
   @spec generate_latex(BillRequest.t()) :: String.t()
   defp generate_latex(bill_request) do
     """
-    #{latex_styler(bill_request.config)}\\usepackage{longtable}
+    #{latex_styler(bill_request.config)}\\usepackage{longtable}\\usepackage{array}
     \\address{#{String.replace(bill_request.bill.seller, ",", ", \\\\ \n")}}
     \\begin{document}
     \\begin{letter}
     {#{String.replace(bill_request.bill.purchaser, ",", ", \\\\ \n")}}
     \\opening{}
     \\begin{center}
-    \\begin{longtable}{| p{7cm} | l | l | l |}
+    \\renewcommand{\\arraystretch}{2}
+    \\begin{longtable}{ p{3cm} p{3cm} p{3cm} p{3cm} p{3cm} }
     \\hline
-    Description & Quantity & Price & Amount \\\\ \\hline
+    \\multicolumn{2}{>{\\centering}p{3cm}}{\\textbf{Product}} & \\multicolumn{1}{>{\\centering}p{3cm}}{\\textbf{Quantity}} & \\multicolumn{1}{>{\\centering}p{3cm}}{\\textbf{Price}} & \\multicolumn{1}{>{\\centering}p{3cm}}{\\textbf{Amount}} \\\\ \\hline
     """ <>
       format_bill(bill_request.bill.products, bill_request.bill.total) <>
       """
       \\end{longtable}
+      \\renewcommand{\\arraystretch}{1}
       \\end{center}
       \\closing{Seal or signature:}
       \\end{letter}
@@ -54,13 +56,17 @@ defmodule BillsGenerator.Filters.LatexFormatter do
 
   defp do_format_bill(acc, [], total) do
     acc <>
-      "\\multicolumn{3}{|c|}{TOTAL} & #{:erlang.float_to_binary(total * 1.0, decimals: 2)} \\\\ \\hline\n"
+      """
+      \\multicolumn{2}{c}{} & \\multicolumn{1}{c}{\\textbf{Dto.}} & \\multicolumn{1}{c}{XX\\%} & \\multicolumn{1}{c}{-5} \\\\ \\cline{3-5}
+      \\multicolumn{2}{c}{} & \\multicolumn{1}{c}{\\textbf{IVA}} & \\multicolumn{1}{c}{XX\\%} & \\multicolumn{1}{c}{5} \\\\ \\cline{3-5}
+      \\multicolumn{2}{c}{} & \\multicolumn{2}{c}{\\textbf{TOTAL}} & \\multicolumn{1}{c}{#{:erlang.float_to_binary(total * 1.0, decimals: 2)}} \\\\ \\cline{3-5}
+      """
   end
 
   defp do_format_bill(acc, [product | t], total) do
     do_format_bill(
       acc <>
-        "#{product.name} & #{product.quantity} & #{:erlang.float_to_binary(product.price * 1.0, decimals: 2)} & #{:erlang.float_to_binary(product.total * 1.0, decimals: 2)} \\\\ \\hline \n",
+        "\\multicolumn{2}{p{3cm}}{#{product.name}} & \\multicolumn{1}{>{\\centering}p{3cm}}{#{product.quantity}} & \\multicolumn{1}{>{\\centering}p{3cm}}{#{:erlang.float_to_binary(product.price * 1.0, decimals: 2)}} & \\multicolumn{1}{>{\\centering}p{3cm}}{#{:erlang.float_to_binary(product.total * 1.0, decimals: 2)}} \\\\ \\hline \n",
       t,
       total
     )
